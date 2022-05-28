22fd4383590b211ce3ed7171b5d30d2824a0cca2
Pred1993 <Mertash.A70@gmail.com>
2022-05-28 13:12:16 +0300

    mikrotaski useState

--- mikrotaski/src/App.tsx
+++ mikrotaski/src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {Button} from "./components/Button";
 
 function App() {
-    // const myFirstSubscriber = (event: React.MouseEvent<HTMLButtonElement>) => {
-    //     console.log('Hellow I am Vasya')
+    // const Button1Foo = (subscriber: string, age: number) => {
+    //     console.log(subscriber, age)
     // }
-    // const myTwoSubscriber = (event: React.MouseEvent<HTMLButtonElement>) => {
-    //     console.log('Hellow I am Artem')
+    // const Button2Foo = (subscriber: string) => {
+    //     console.log(subscriber)
+    // }
+    // const Button3Foo = () => {
+    //     console.log('I am stupid button')
+    // }
+    let [a, setA] = useState(1)
+    const onClickHandler = () => {
+        setA(++a)
+    }
+    const numbing = () => {
+        setA(a = 0)
+    }
+    return (
+        <div className='App'>
+            {/*<Button name={'MyYouTubeChanel-1'} callBack={() => Button1Foo('I am Vasya', 21)}/>*/}
+            {/*<Button name={'MyYouTubeChanel-2'} callBack={() => Button2Foo('I am Ivan')}/>*/}
+            {/*<Button name={'MyYouTubeChanel-3'} callBack={Button3Foo}/>*/}
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
+            <button onClick={numbing}>0</button>
+        </div>
+    )
 
-    const onClickHandler = (name: string) => {
-        console.log(name)
-    }
-        return (
-            <div className='App'>
-                {/*<button onClick={(event) => {console.log('Hellow')}}>MyYouTubeChanel-1</button>*/}
-                <button onClick={(event) => onClickHandler("Artem")}>MyYouTubeChanel-1</button>
-                <button onClick={(event) => onClickHandler('Vasya')}>MyYouTubeChanel-2</button>
-            </div>
-        )
-    }
+}
 
 
 export default App
---
+++ mikrotaski/src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type ButtonPropsType = {
+    name: string,
+    callBack: () => void
+}
+export const Button = (props: ButtonPropsType) => {
+    const OnClickHandler = () => {
+        props.callBack()
+    }
+    return (
+        <button onClick={OnClickHandler}>{props.name}</button>
+    );
+};
+
+// event: React.MouseEvent<HTMLButtonElement>
